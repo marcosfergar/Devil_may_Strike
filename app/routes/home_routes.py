5650317283640e09b78db17a24ac2629710bd1c9
marcos <mfg99995@gmail.com>
2026-04-17 10:37:06 +0200

    fix-plantillas-usuario

--- app/routes/home_routes.py
+++ app/routes/home_routes.py
@@ -10,6 +10,7 @@ from flask import Blueprint, redirect, render_template, session, url_for
 # from app.forms.trainer_form import TrainerForm
 
 # importe sercicios
+from app.services import usuario_service
 from app.services.rawg_service import listar_saga_dmc
 
 home_pb = Blueprint('home_route', __name__, template_folder='templates')
@@ -19,16 +20,22 @@ home_pb = Blueprint('home_route', __name__, template_folder='templates')
 def paginaBienvenida():
     if "username" not in session:
             return redirect(url_for('homeLogin_route.paginaLogin'))
-        
-    return render_template('home.html', usuario=session.get("username"))
+    
+    nombre_sesion = session.get("username")
+    user = usuario_service.obtener_usuario_por_nombre(nombre_sesion)
+
+    return render_template('home.html', usuario=user)
 
 @home_pb.route('/biblioteca-dmc')
 def biblioteca_dmc():
     if "username" not in session:
             return redirect(url_for('homeLogin_route.paginaLogin'))
     
+    nombre_sesion = session.get("username")
+    user = usuario_service.obtener_usuario_por_nombre(nombre_sesion)
+
     juegos_saga = listar_saga_dmc()
-    return render_template('biblioteca-dmc.html',usuario=session.get("username"), juegos=juegos_saga)
+    return render_template('biblioteca-dmc.html',usuario=user, juegos=juegos_saga)
 
 @home_pb.route("/logout")
 def logout():
--- app/routes/perfil_routes.py
+++ app/routes/perfil_routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, redirect, render_template, session, url_for
 
+from app.services import usuario_service
+
 # importe de rutas
 
 # importe modelos
@@ -15,6 +17,9 @@ def perfil():
     if "username" not in session:
         return redirect(url_for('homeLogin_route.paginaLogin'))
     
-    return render_template('perfil.html', usuario=session.get("username"))
+    nombre_sesion = session.get("username")
+    user = usuario_service.obtener_usuario_por_nombre(nombre_sesion)
+    
+    return render_template('perfil.html', usuario=user)
 
 
